2388364ad9935aebe98ef9066da4e15db507a326
Kristaps Ritins <kritinsdev@gmail.com>
2021-11-21 23:22:39 +0200

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/restaurants', (req, res) => {
+    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/recommended', (req, res) => {
+    const htmlFilePath = path.join(__dirname, 'views', 'recommended.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/about', (req, res) => {
+    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/confirm', (req, res) => {
+    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.listen(port);
+
