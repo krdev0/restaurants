fc27ab1f739c078131c4135adf2ef45bf2243fdd
Kristaps Ritins <kritinsdev@gmail.com>
2021-11-24 22:55:03 +0200

    static files route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     const htmlFilePath = path.join(__dirname, 'views', 'index.html');
     res.sendFile(htmlFilePath);
