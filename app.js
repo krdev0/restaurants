695832ee7351280e51d89ac112e8e8fe2b0e43d0
Kristaps Ritins <kritinsdev@gmail.com>
2021-11-24 23:15:40 +0200

    add restaurant info logic

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
+const { json } = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -19,6 +22,21 @@ app.get('/restaurants', (req, res) => {
 app.get('/recommended', (req, res) => {
     const htmlFilePath = path.join(__dirname, 'views', 'recommended.html');
     res.sendFile(htmlFilePath);
+});
+
+app.post('/recommended', (req, res) => {
+    const restaurantData = req.body;
+    const jsonFilePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(jsonFilePath);
+
+    const storedRestaurants = JSON.parse(fileData);
+
+    storedRestaurants.push(restaurantData);
+
+    fs.writeFileSync(jsonFilePath, JSON.stringify(storedRestaurants));
+
+    res.redirect('/confirm');
 });
 
 app.get('/about', (req, res) => {
