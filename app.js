2b84d091af2e9621a3d1974904fc821b038b26b5
Kristaps Ritins <kritinsdev@gmail.com>
2021-11-29 23:38:29 +0200

    added status codes for responses

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get('/restaurants/:id', (req, res) => {
             });
         }
     }
-    // res.render('404');
+    res.status(404).render('404');
 });
 
 app.get('/recommended', (req, res) => {
@@ -82,11 +82,11 @@ app.get('/confirm', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.render('404')
+    res.status(404).render('404')
 });
 
 app.use((error, req, res, next) => {
-    res.render('500');
+    res.status(500).render('500');
 });
 
 app.listen(port);
